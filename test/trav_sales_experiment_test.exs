bc4cf8297b3db657644427fc50621519ab92c495
Brad Heintz <brad.heintz@gmail.com>
2023-01-27 14:42:19 -0800

    fixed some oopses

--- test/trav_sales_experiment_test.exs
+++ test/trav_sales_experiment_test.exs
@@ -5,6 +5,7 @@ defmodule TravSalesExperimentTest do
   @test_domain_size 5
   @test_keep_portion 0.4
   @test_population_size 300
+  @test_map %CityMap{cities: [[0.0, 0.0], [0.1, 0.0], [0.2, 0.0]]}
 
   test "exposes a function to give a single population member" do
     test_domain_size = 5
@@ -16,7 +17,6 @@ defmodule TravSalesExperimentTest do
 
   test "exposes a function to give a new domain instance" do
     test_domain_size = 5
-    # TODO experiment framework currently dumb about domain, so only parametrizes by size
     test_result = TravSalesExperiment.new_domain(test_domain_size)
     # TODO right now, this is a very specific type - not sure how to make a generic "domain", but need to keep framework dumb about this type
     assert %CityMap{} = test_result
@@ -24,29 +24,27 @@ defmodule TravSalesExperimentTest do
   end
 
   test "exposes a function that scores a individual against the domain" do
-    test_map = %CityMap{cities: [[0.0, 0.0], [0.1, 0.0], [0.2, 0.0]]}
     test_individual = %Individual{genes: [0,0,0]} # traverses the three cities in list order
 
-    test_result = TravSalesExperiment.score(test_individual, test_map)
+    test_result = TravSalesExperiment.score(test_individual, @test_map)
     assert %Individual{} = test_result
     assert_in_delta test_result.fitness, 2.62842712, @test_delta
 
     # double-checking for non-trivial route
     test_individual2 = %Individual{genes: [0,1,0]}
 
-    test_result = TravSalesExperiment.score(test_individual2, test_map)
+    test_result = TravSalesExperiment.score(test_individual2, @test_map)
     assert_in_delta test_result.fitness, 2.52842712, @test_delta
   end
 
-  # TODO this means we can only mutate babies
+  # NB this means we can only mutate babies
   test "the scoring function should skip already-scored individuals" do
-    test_map = %CityMap{cities: [[0.0, 0.0], [0.1, 0.0], [0.2, 0.0]]}
-    # NB this score is impossible and will verify the skip
-    test_individual = %Individual{genes: [0,0,0], fitness: 321.0} # traverses the three cities in list order
+    impossible_score = 321.0 # verifies skip
+    test_individual = %Individual{ genes: [0,0,0], fitness: impossible_score }
 
-    test_result = TravSalesExperiment.score(test_individual, test_map)
+    test_result = TravSalesExperiment.score(test_individual, @test_map)
     assert %Individual{} = test_result
-    assert_in_delta test_result.fitness, 321.0, @test_delta
+    assert_in_delta test_result.fitness, impossible_score, @test_delta
   end
 
   # distance calculations
@@ -70,5 +68,11 @@ defmodule TravSalesExperimentTest do
   # reproduction
   test "exposes a function that takes two population members and returns a third" do
     flunk("assert something here")
+  end
+
+  # mutation
+  test "exposes a function to mutate genes of a population member" do
+    # only do for unscored members
+    # TODO
   end
 end
